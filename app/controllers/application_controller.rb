98bb731ad0a42d5724148e385ca6dd0c9e23ef6f
umar482 <umar.omtomo@gmail.com>
2021-06-10 15:32:54 +0500

    Created a basic level auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def default_action
+    redirect_to resolve_root_url
+  end
+
+  def resolve_root_url
+    if user_signed_in?
+      posts_path
+    else
+      new_user_session_path
+    end
+  end
 end
